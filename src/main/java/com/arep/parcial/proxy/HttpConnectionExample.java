b5c5129e0fc4b051b321c0ed0e1b47eb8bfd437c
unknown <santiago.coronado-p@labinfo.is.escuelaing.edu.co>
2025-10-21 09:12:30 -0500

    proxy build

---
+++ src/main/java/com/arep/parcial/proxy/HttpConnectionExample.java
@@ -0,0 +1,44 @@
+package com.arep.parcial.proxy;
+
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+@Service
+public class HttpConnectionExample {
+
+    private final String USER_AGENT = "Mozilla/5.0";
+
+    public String get(String url) throws IOException {
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            System.out.println(response.toString());
+            return response.toString();
+
+        } else {
+            return "GET request not worked";
+        }
+    }
+}
---
+++ src/main/java/com/arep/parcial/proxy/controller/proxyController.java
@@ -0,0 +1,47 @@
+package com.arep.parcial.proxy.controller;
+
+import com.arep.parcial.proxy.HttpConnectionExample;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@RestController
+public class proxyController {
+
+    int iterador = 0;
+
+    String[] urls = {"http://localhost:8080" , "http://localhost:8080"};
+
+    @Autowired
+    HttpConnectionExample httpConnectionExample;
+
+    @RequestMapping(value = "/**")
+    public ResponseEntity<?> getProxy(HttpServletRequest httpServletRequest){
+        try{
+
+            iterador = (iterador + 1) % urls.length;
+
+            String server = urls[iterador];
+
+            String path = httpServletRequest.getContextPath();
+
+            String query = httpServletRequest.getQueryString() != null ? "?" + httpServletRequest.getQueryString() : "";
+
+            String url = server + path + query;
+
+            String response = httpConnectionExample.get(url);
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(response);
+        }
+        catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getCause());
+        }
+    }
+}
